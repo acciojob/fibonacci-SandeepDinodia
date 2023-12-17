d32554df5d424b203377149fd3cdac41fb5e8ef2
SandeepDinodia <151106767+SandeepDinodia@users.noreply.github.com>
2023-12-17 22:59:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 function fibonacci(num) {
 // your code here
+	
+    if num <= 0:
+        return "Invalid input. Please provide a positive integer greater than 0."
+    elif num == 1:
+        return 0
+    elif num == 2:
+        return 1
+    else:
+        a, b = 0, 1
+        for _ in range(num - 2):
+            a, b = b, a + b
+        return b
 }
 
 module.exports = fibonacci;
